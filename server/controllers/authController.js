0e7b91532ecc83dccdfb260cfdd0bbd623f227be
Waghib <Waghibahmad30@gmail.com>
2025-04-20 23:40:44 +0500

    added auth routes and tested api endpoints

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -15,6 +15,7 @@ export const register = async (req, res) => {
         const existingUser = await userModel.findOne({ email})
 
         if (existingUser) {
+            console.log("User already exists");
             return res.json({
                 status: false,
                 message: "User already exists",
@@ -39,6 +40,7 @@ export const register = async (req, res) => {
             sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         });
+        console.log("User registered successfully");
         return res.json({
             status: true,
             message: "Registration successful",
@@ -84,11 +86,32 @@ export const login = async (req, res) => {
             sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         });
+        console.log("User logged in successfully");
         return res.json({
             status: true,
             message: "Login successful",
         });
 
+    } catch (error) {
+        return res.json({
+            status: false,
+            message: error.message,
+        });
+    }
+}
+
+export const logout = async (req, res) => {
+    try {
+        res.clearCookie("token", {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        });
+        console.log("User logged out successfully");
+        return res.json({
+            status: true,
+            message: "Logout successful",
+        });
     } catch (error) {
         return res.json({
             status: false,
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import 'dotenv/config';
 import cookieParser from 'cookie-parser';
 import connectDB from './config/mongodb.js';
+import authRouter from './routes/authRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -17,9 +18,13 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({credentials: true}));
 
+// API Endpoints
 app.get('/', (req, res) => {
-    res.send('API is running.');
+  res.send('API is running.');
 });
+app.use('/api/auth', authRouter);
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);

57fc532951b7b2bc0930223c53fc6c52e5e2257f
Waghib <Waghibahmad30@gmail.com>
2025-05-11 18:12:10 +0500

    login page completed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Navbar from '../components/Navbar.jsx'
+import Header from '../components/Header.jsx'
+import { assets } from '../assets/assets.js'
 
 const Home = () => {
   return (
-    <div>Home page</div>
+    <div 
+      className='flex flex-col items-center justify-center min-h-screen bg-cover bg-center'
+      style={{ backgroundImage: `url(${assets.bg_img})` }}
+    >
+        <Navbar />
+        <Header />
+    </div>
   )
 }
 
